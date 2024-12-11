fcd04f472a549adf60c74475fc93bdd8e47b7df0
Suhrob <Suhrob@gmail.com>
2024-12-11 14:59:01 +0500

    tugadi

---
+++ app.js
@@ -0,0 +1,27 @@
+// 1
+let a = prompt("son kiriting");
+
+if (a > 0) {
+    console.log("musbat.");
+} else {
+    console.log("manfiy");
+}
+//2
+let a1 = +prompt("son kiriting");
+console.log(a1 % 2 !== 0);
+
+// 3
+let a2 = +prompt("a son kiriting");
+let b2 = +prompt("b son kiriting");
+console.log(a2 > 2 && b2 <= 3);
+
+// 4
+let a3 = +prompt("a3 son kiriting");
+let b3 = +prompt("b3 son kiriting");
+let c1 = +prompt("c1 son kiriting");
+console.log(a3 <= b3 && b3 <= C);
+
+//5
+let a4 = +prompt("a4 son kiriting");
+let b4 = +prompt("b4 son kiriting");
+console.log(a4 % 2 === b4 % 2);
